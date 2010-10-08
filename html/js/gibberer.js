7ea4d3e8b29f400d55022ec73169f9730d134173
Stephen Swift <stephen.swift@gmail.com>
2010-10-08 17:15:38 -0400

    Added jQuery UI, changed HTML to allow for multiple windows, prettied CSS up a bit, etc

--- html/js/gibberer.js
+++ html/js/gibberer.js
@@ -1,26 +1,32 @@
 $(document).ready(function() {
-    var client = new Faye.Client('http://mole.local:8000/');
+    var client = new Faye.Client('http://localhost:8000/');
     // receive chat messages
 
     client.subscribe('/messages', function(message) {
-        var p = $("<div class='chat' style='display:block;'><div class='content'><span class='username'>" + message.user + ": </span>" + message.text + "</div></div>");
-        $('#chats').append(p);
-        $('#chats').scrollTop($('#chats').attr("scrollHeight"));
+        var p = $("<div class='chatLine' style='display:block;'><div class='chatLineContent'><span class='chatLineUsername'>" + message.user + ": </span>" + message.text + "</div></div>");
+		console.log(p);
+        $('.chats').append(p);
+        $('.chats').scrollTop($('.chats').attr("scrollHeight"));
     });
 
-    $("#pubmsg").keyup(function(event) {
-            if ((event.keyCode == '13' || event.keyCode == undefined) && $("#pubmsg").val()) {
-            client.publish('/messages', {
-                      text: $("#pubmsg").val(),
-                      user: $("#username").val() || "Unknown",
-                    });
-                    $("#pubmsg").val("").focus();
-            }
+    $(".pubmsg").keyup(function(event) {
+		console.log($(this).closest('.chatContainer').find('.username').val());
+		if ((event.keyCode == '13' || event.keyCode == undefined) && $(this).val()) {
+			client.publish('/messages', {
+				text: $(this).val(),
+                user: $(this).closest('.chatContainer').find('.username').val() || "Unknown",
+			});
+	
+			$(this).val("").focus();
+		}
     });
 
-    $('#pubsend').click(function() {
-      $('#pubmsg').keyup();
+    $('.pubsend').click(function() {
+		$(this).closest('.pubmsg').keyup();
     });
+
+	// JQUERY UI AWAYYYYYYYYY
+	$( ".chatContainer" ).draggable({ stack: ".chatContainer" });
 
 });
 
